41d3f5c25757c66b8737bcbbe635135993132008
apuroop08 <apuroopkumar8143@gmail.com>
2025-12-18 15:22:23 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const app = express();
+const usermodel = require("./models/user");
+const postmodel = require("./models/post");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.post("/create", async (req, res) => {
+    let {username, name, email, password} = req.body;
+    let user = await usermodel.findOne({email});
+    if(user) return res.status(500).send("User Already Exists"); 
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) => {
+            let user = await usermodel.create({
+                username,
+                name,
+                email,
+                password: hash
+            }); 
+
+            let token = jwt.sign({emial: email, userid: user._id}, "shhhhh");
+            res.cookie("token", token);
+            res.send("registered");
+        }) 
+    });
+});
+
+app.post("/login", async (req,res) => {
+    let {email, password} = req.body;
+    let user = await usermodel.findOne({email});
+    if(!user) return res.status(500).send("Something Went Wrong"); 
+
+    bcrypt.compare(password, user.password, (err, result) => {
+        if(result) {
+            let token = jwt.sign({email: email, userid: user._id}, "shhhhh");
+            res.cookie("token", token);
+            res.status(200).redirect("/profile");
+        }
+        else res.redirect("/login");
+    });
+});
+
+app.get("/logout", (req, res) => {
+    res.cookie("token","");
+    res.redirect("/login");
+});
+
+app.get("/profile", isLoggedIn, async (req, res) => {
+    let user = await usermodel.findOne({email: req.user.email}).populate("posts");
+    res.render("profile", {user});
+});
+
+app.post("/post", isLoggedIn, async (req, res) => {
+    let user = await usermodel.findOne({email: req.user.email});
+    let post = await postmodel.create({
+        user: user._id,
+        content: req.body.content,
+    });
+
+    user.posts.push(post._id);
+    await user.save();
+    res.redirect("/profile");
+});
+
+app.get("/like/:id", isLoggedIn, async (req, res) => {
+    let post = await postmodel.findOne({_id: req.params.id}).populate("user");
+    if(post.likes.indexOf(req.user.userid) === -1) {
+         post.likes.push(req.user.userid);
+    }
+    else {
+        post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+    }
+    await post.save();
+    res.redirect("/profile");
+});
+
+app.get("/delete/:id", isLoggedIn, async (req, res) => {
+    await postmodel.findOneAndDelete({_id: req.params.id});
+    res.redirect("/profile");
+});
+
+function isLoggedIn(req, res, next) {
+    if(req.cookies.token === "")  res.send("You have to loggin");
+    else {
+        let data = jwt.verify(req.cookies.token, "shhhhh");
+        req.user = data;
+        next();
+    }
+}
+
+app.listen(3000);
